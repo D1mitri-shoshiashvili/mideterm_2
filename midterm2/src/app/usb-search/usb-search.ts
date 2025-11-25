5993b3646df6e88ba197c1c85de5bedfccfa948f
D1mitri-shoshiashvili <dimitrishoshiashvili@ibsu.edu.ge>
2025-11-25 15:54:53 +0400

    3.2 is done

--- midterm2/src/app/usb-search/usb-search.ts
+++ midterm2/src/app/usb-search/usb-search.ts
@@ -1,16 +1,23 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { shopItem } from '../models/shop-item';
 import { ShopService } from '../shop-service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-usb-search',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './usb-search.html',
   styleUrl: './usb-search.css',
 })
 export class UsbSearch implements OnInit{
   shopService = inject(ShopService);
   searchResults = signal<shopItem[]>([]);
+
+  searchForm = new FormGroup({
+    model: new FormControl(''),
+    priceFrom: new FormControl<number | null>(null),
+    priceTo: new FormControl<number | null>(null)
+  });
 
   ngOnInit(): void {
     let allItems = this.shopService.getItems();

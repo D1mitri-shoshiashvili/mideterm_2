b96682cb891e6324dad444beeb8e14903739a354
D1mitri-shoshiashvili <dimitrishoshiashvili@ibsu.edu.ge>
2025-11-25 16:28:41 +0400

    4.1 is done

---
+++ midterm2/src/app/add-item/add-item.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-item',
+  imports: [ReactiveFormsModule],
+  templateUrl: './add-item.html',
+  styleUrl: './add-item.css',
+})
+export class AddItem {
+  form = new FormGroup({
+    modelName: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50)
+    ]),
+    price: new FormControl< | null>(null,[
+      Validators.required,
+
+      Validators.min(0)
+    ]),
+    isAvaliable: new FormControl(false)
+  });
+}
